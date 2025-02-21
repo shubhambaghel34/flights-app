c905cc48341451de45c1a21934916ca0558fa222
Shubham Baghel <shubhambaghel34@gmail.com>
2025-02-21 18:58:10 +0530

    updated code

--- src/App.js
+++ src/App.js
@@ -1,37 +1,6 @@
 
 import './App.css';
-import AirportsList from './componets/Airports/AirportList';
-import FlightsList from './componets/Flights/FlightList';
-import { useState,useEffect } from 'react';
 import FlightTable from './componets/FlightTable/FlightTable';
-// const App = () => {
-//   const [activeTab, setActiveTab] = useState("flights");
-
-//   return (
-//     <div className="app-container">
-//       <div className="tabs">
-//         <button 
-//           className={activeTab === "flights" ? "active" : ""}
-//           onClick={() => setActiveTab("flights")}
-//         >
-//           Flights
-//         </button>
-//         <button 
-//           className={activeTab === "airports" ? "active" : ""}
-//           onClick={() => setActiveTab("airports")}
-//         >
-//           Airports
-//         </button>
-//       </div>
-
-//       <div className="content">
-//         {activeTab === "flights" ? <FlightsList /> : <AirportsList />}
-//       </div>
-//     </div>
-//   );
-// };
-
-
 const App = () => {
   return (
     <div className="p-5">
